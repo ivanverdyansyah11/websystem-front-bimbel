6e07844bb886cd0e0aaa25071da8d7f514a932f7
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2023-12-16 01:25:23 +0800

    Progress: Finish Topbar

---
+++ resources/views/components/topbar.blade.php
@@ -0,0 +1,9 @@
+<div class="topbar d-flex align-items-center justify-content-between">
+    <h3>Explore Dashboard</h3>
+    <div class="topbar-wrapper d-flex align-items-center gap-3">
+        <div class="notification-wrapper d-flex align-items-center justify-content-center">
+            <div class="notification-icon"></div>
+        </div>
+        <img src="{{ asset('assets/images/profile/profie-image.svg') }}" alt="Profile Image" class="img-fluid d-none d-md-inline-block" width="46">
+    </div>
+</div>
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="col-12 col-lg-10">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A officiis sunt laboriosam distinctio vero! Maiores officiis, fuga consequuntur voluptatibus dolorum qui, quaerat est, ab magnam tenetur sint numquam voluptatem laudantium.</p>
+    <div class="col-12 col-lg-10 p-0">
+        @include('components.topbar')
+        {{-- @include('components.filter') --}}
+        <p class="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. A officiis sunt laboriosam distinctio vero! Maiores officiis, fuga consequuntur voluptatibus dolorum qui, quaerat est, ab magnam tenetur sint numquam voluptatem laudantium.</p>
     </div>
 @endsection
