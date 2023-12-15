79966d81fdbf2517a8e5e7124564791d60e2b8b7
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2023-12-16 02:01:16 +0800

    Progress: Finish Filter

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,18 @@
 const hamburger = document.querySelector('.hamburger');
 const sidebar = document.querySelector('.sidebar');
+const categoryButton = document.querySelector('.category-button');
+const categoryList = document.querySelector('.category-list');
+const sortButton = document.querySelector('.sort-button');
+const sortList = document.querySelector('.sort-list');
 
 hamburger.addEventListener('click', function() {
     sidebar.classList.toggle('active');
+});
+
+categoryButton.addEventListener('click', function() {
+    categoryList.classList.toggle('active');
+});
+
+sortButton.addEventListener('click', function() {
+    sortList.classList.toggle('active');
 });
--- resources/views/components/filter.blade.php
+++ resources/views/components/filter.blade.php
@@ -1,9 +1,32 @@
-<div class="topbar d-flex align-items-center justify-content-between">
-    <h3>Explore Dashboard</h3>
-    <div class="topbar-wrapper d-flex align-items-center gap-3">
-        <div class="notification-wrapper d-flex align-items-center justify-content-center">
-            <div class="notification-icon"></div>
+<div class="filter d-flex align-items-center justify-content-between">
+    <form class="form search">
+        <div class="search-wrapper position-relative">
+            <input type="text" class="input" placeholder="Search..">
+            <div class="search-icon"></div>
         </div>
-        <img src="{{ asset('assets/images/profile/profie-image.svg') }}" alt="Profile Image" class="img-fluid" width="46">
+    </form>
+    <div class="filter-wrapper d-none d-md-flex align-items-center gap-2">
+        <div class="filter-category position-relative">
+            <div class="category-button d-flex align-items-center gap-2">
+                <div class="category-icon"></div>
+                Category
+            </div>
+            <div class="category-list position-absolute d-flex flex-column" style="gap: 2px;">
+                <a href="#" class="list-item">Pancasila</a>
+                <a href="#" class="list-item">Kalkulus</a>
+                <a href="#" class="list-item">Matematika</a>
+            </div>
+        </div>
+        <div class="filter-sort position-relative">
+            <div class="sort-button d-flex align-items-center gap-2">
+                <div class="sort-icon"></div>
+                Sort By: Deadline
+            </div>
+            <div class="sort-list position-absolute d-flex flex-column" style="gap: 2px;">
+                <a href="#" class="list-item">Pancasila</a>
+                <a href="#" class="list-item">Kalkulus</a>
+                <a href="#" class="list-item">Matematika</a>
+            </div>
+        </div>
     </div>
 </div>
