35a1ed5eca20e2de8678698e63b62e90eba024ac
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2023-12-17 03:04:22 +0800

    Progress: Finish Register

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -16,9 +16,13 @@
         <div class="row justify-content-center">
             <div class="col-md-10 col-lg-6 col-xl-5">
                 <div class="card-auth">
-                    <h2 class="auth-title">Login</h2>
+                    <h2 class="auth-title">Register</h2>
                     <form class="form">
                         <div class="row">
+                            <div class="col-12 input-wrapper mb-3">
+                                <label for="username">Username</label>
+                                <input type="username" id="username" class="input" placeholder="Input username..">
+                            </div>
                             <div class="col-12 input-wrapper mb-3">
                                 <label for="email">Email Address</label>
                                 <input type="email" id="email" class="input" placeholder="Input email address..">
@@ -30,12 +34,12 @@
                         </div>
                         <div class="row">
                             <div class="col-12 d-flex gap-2">
-                                <button type="submit" class="button-primary w-100">Login Account</button>
+                                <button type="submit" class="button-primary w-100">Create an Account</button>
                             </div>
                         </div>
                         <div class="row mt-4">
                             <div class="col-12 text-center">
-                                <p class="link-redirect">Don't have an account? <a href="{{ route('register') }}">Create an Account</a></p>
+                                <p class="link-redirect">Already have an account? <a href="{{ route('login') }}">Login Account</a></p>
                             </div>
                         </div>
                     </form>
